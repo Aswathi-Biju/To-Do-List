66225d024c0318d96f0bad31a1f6da0be859cf1c
Aswathi-Biju <aswathibiju8118@gmail.com>
2025-09-04 21:54:39 +0530

    Initial React project commit

---
+++ to-do-list app/src/TodoList.jsx
@@ -0,0 +1,12 @@
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+    {todos.length === 0 && "No Todos"}
+    {todos.map(todo => {
+      return (
+      <TodoList {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    )
+    })}
+  </ul>
+    )
+}
